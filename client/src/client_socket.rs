66a7dff88a85021f7c752eaf8db73e530e608e09
Tomasz Sterna <tomek@sterna.link>
2020-09-29 01:09:27 +0200

    Make ClientSocketTrait Send + Sync when multithreaded

--- client/src/client_socket.rs
+++ client/src/client_socket.rs
@@ -4,18 +4,36 @@ use naia_socket_shared::LinkConditionerConfig;
 
 use super::{error::NaiaClientSocketError, message_sender::MessageSender, packet::Packet};
 
-/// Defines the functionality of a Naia Client Socket
-pub trait ClientSocketTrait: Debug {
-    /// Receive a new packet from the socket, or a tick event
-    fn receive(&mut self) -> Result<Option<Packet>, NaiaClientSocketError>;
-    /// Gets a MessageSender you can use to send messages through the Server
-    /// Socket
-    fn get_sender(&mut self) -> MessageSender;
-    /// Wraps the current socket in a LinkConditioner
-    fn with_link_conditioner(
-        self: Box<Self>,
-        config: &LinkConditionerConfig,
-    ) -> Box<dyn ClientSocketTrait>;
+cfg_if! {
+    if #[cfg(feature = "multithread")] {
+        /// Defines the functionality of a Naia Client Socket
+        pub trait ClientSocketTrait: Debug + Send + Sync {
+            /// Receive a new packet from the socket, or a tick event
+            fn receive(&mut self) -> Result<Option<Packet>, NaiaClientSocketError>;
+            /// Gets a MessageSender you can use to send messages through the Server
+            /// Socket
+            fn get_sender(&mut self) -> MessageSender;
+            /// Wraps the current socket in a LinkConditioner
+            fn with_link_conditioner(
+                self: Box<Self>,
+                config: &LinkConditionerConfig,
+            ) -> Box<dyn ClientSocketTrait>;
+        }
+    } else {
+        /// Defines the functionality of a Naia Client Socket
+        pub trait ClientSocketTrait: Debug {
+            /// Receive a new packet from the socket, or a tick event
+            fn receive(&mut self) -> Result<Option<Packet>, NaiaClientSocketError>;
+            /// Gets a MessageSender you can use to send messages through the Server
+            /// Socket
+            fn get_sender(&mut self) -> MessageSender;
+            /// Wraps the current socket in a LinkConditioner
+            fn with_link_conditioner(
+                self: Box<Self>,
+                config: &LinkConditionerConfig,
+            ) -> Box<dyn ClientSocketTrait>;
+        }
+    }
 }
 
 cfg_if! {
